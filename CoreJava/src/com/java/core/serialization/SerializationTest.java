0ce8d3cb0e71cda5a24bf4800b2d13bba12a7a8a
hima <hima@hima-VAIO>
2020-04-02 18:13:10 +0530

    Serialization basic

---
+++ CoreJava/src/com/java/core/serialization/SerializationTest.java
@@ -0,0 +1,20 @@
+package com.java.core.serialization;
+
+public class SerializationTest 
+{
+	public static void main(String[] args) 
+	{
+		String fileName="employee.ser";
+		Employee emp = new Employee();
+		emp.setId(100);
+		emp.setName("Emp1");
+		emp.setSalary(5000);
+		
+		//SerializationUtil.serialize(emp, fileName);
+		
+		Employee empNew = (Employee) SerializationUtil.deserialize(fileName);
+		
+		System.out.println("emp Object::" + emp);
+		System.out.println("empNew Object::" + empNew);
+	}
+}
---
+++ CoreJava/src/com/java/core/serialization/SerializationUtil.java
@@ -0,0 +1,42 @@
+package com.java.core.serialization;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SerializationUtil 
+{
+	// serialize the given object and save it to file
+	public static void serialize(Object obj, String fileName)
+	{
+		try(FileOutputStream fos = new FileOutputStream(fileName);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);)
+		{
+			oos.writeObject(obj);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+
+	// deserialize to Object from given file
+	public static Object deserialize(String fileName) 
+	{
+		Object obj = null;
+				
+		try(FileInputStream fis = new FileInputStream(fileName);
+			ObjectInputStream ois = new ObjectInputStream(fis);)
+		{
+			obj = ois.readObject();
+		} 
+		catch (IOException | ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return obj;
+	}
+}
