d35031c77ffa35ec7a21094318d48ec26ba7166a
Tejas Jethva <tejasmjethva@gmail.com>
2020-04-03 08:16:24 +0530

    initial

---
+++ JavaInterview/src/com/java/core/ImmutableTest.java
@@ -0,0 +1,23 @@
+package com.java.core;
+
+public class ImmutableTest 
+{
+
+}
+
+class Employee
+{
+	String name;
+	
+	Address address;
+}
+
+//3rd party class
+class Address
+{
+	String area;
+	
+	int pincode;
+	
+	//setters & getters
+}
